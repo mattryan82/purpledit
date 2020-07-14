30f35f3ebda3572aee3ab2bdc231b5e8bb996d06
Matt Ryan <mattryanjersey@gmail.com>
2020-07-14 19:28:13 +0100

    added the comment scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
-  resources :posts
+  
+  resources :comments
+  resources :posts do
+    resources :comments, only: [:create, :update, :destroy]
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 
-  get "/home" => "static#home"
+  #get "/home" => "static#home"
 
 end
 
