a5c17784d7e937525226a004fd2cd72200c38564
ChelleOost <chelleoost@gmail.com>
2020-07-16 19:26:19 +0100

    linkdbtable

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_14_182719) do
+ActiveRecord::Schema.define(version: 2020_07_16_182304) do
 
   create_table "comments", force: :cascade do |t|
     t.text "comment_body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "post_id"
   end
 
   create_table "posts", force: :cascade do |t|
