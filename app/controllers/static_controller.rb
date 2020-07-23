95c2c729ac41308f822dc3c86bf5fea5d699e15a
ChelleOost <chelleoost@gmail.com>
2020-07-23 20:16:03 +0100

    homepage set up. Posts still not showing after cahnging to listposts

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,7 +1,7 @@
 class StaticController < ApplicationController
   def static
     @home = Home
-    @posts = Post.order("created_at desc").limit(2)
+    @listposts = Post.order("created_at desc").limit(2)
 
   end
 end
