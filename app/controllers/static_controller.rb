fa11972a55986fd34b4fe30a8322379b96c96f0a
mattryan82 <mattryanjersey@gmail.com>
2020-07-28 14:38:31 +0100

    fixed homepage to list posts with a link to open them

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,7 +1,14 @@
 class StaticController < ApplicationController
   def static
     @home = Home
-    @listposts = Post.order("created_at desc").limit(2)
+  #  @listposts = Post.order("created_at desc").limit(2)
 
   end
+
+  # created the home method which is called when the "home_path" path/url is used
+  def home
+    @listposts = Post.order("created_at desc").limit(2)
+  end
+
+
 end
