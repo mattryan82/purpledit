2dc7b89e4deb775fe8a27260022fd465520ccd5a
mattryan82 <mattryanjersey@gmail.com>
2020-07-30 19:05:59 +0100

    added user_id field to posts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_28_175326) do
+ActiveRecord::Schema.define(version: 2020_07_30_173812) do
 
   create_table "comments", force: :cascade do |t|
     t.text "comment_body"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 2020_07_28_175326) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "content"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
