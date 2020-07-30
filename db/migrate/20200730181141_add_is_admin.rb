22e725c015b2337ce5dba65851b2a17c8f542b4b
mattryan82 <mattryanjersey@gmail.com>
2020-07-30 19:24:32 +0100

    added one-to-many relationship statements to models, belongs_to etc...

--- db/migrate/20200730181141_add_is_admin.rb
+++ db/migrate/20200730181141_add_is_admin.rb
@@ -1,4 +1,6 @@
 class AddIsAdmin < ActiveRecord::Migration[6.0]
   def change
+    add_column :users, :is_admin, :boolean
+    add_column :comments, :user_id, :integer
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_30_173812) do
+ActiveRecord::Schema.define(version: 2020_07_30_181141) do
 
   create_table "comments", force: :cascade do |t|
     t.text "comment_body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "post_id"
+    t.integer "user_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
@@ -37,6 +38,7 @@ ActiveRecord::Schema.define(version: 2020_07_30_173812) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "is_admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
