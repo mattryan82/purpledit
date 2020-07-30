f4abb20810ce3a9a518f8aaf6db04c864cf3615a
mattryan82 <mattryanjersey@gmail.com>
2020-07-30 20:24:24 +0100

    added a custom 'not authorised' message when user undertakes an unauthorised action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,19 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name])
+  end
+
+
+  private
+
+  def user_not_authorized
+    flash[:alert] = "You are not authorised to perform this action."
+    redirect_to(request.referrer || root_path)
   end
 end
